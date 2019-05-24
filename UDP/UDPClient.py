1f2d754c6ff553c5fe08e0f181d3904eee61f276
jsevilla274 <jsevilla274@gmail.com>
2019-05-24 18:58:45 -0400

    Add client timeout, RT timing, and server packet loss simulation

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,14 +1,26 @@
 #UDPClient.py
 
-from socket import socket, SOCK_DGRAM, AF_INET
+from socket import socket, SOCK_DGRAM, AF_INET, timeout
+import time
 
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence: ')
-clientSocket.sendto(message, (serverName, serverPort))
-modifiedMessage, addr = clientSocket.recvfrom(2048)
-print modifiedMessage, addr
-clientSocket.close()
+clientSocket.settimeout(1)    # sets timeout to 1 second
+message = input('Input lowercase sentence: ').encode()
+RTT = 0
+try:
+    # send packet to server
+    RTT = int(round(time.time() * 1000))    # start time
+    clientSocket.sendto(message, (serverName, serverPort))
 
-#Allow the client to give up if no response has been reveived within 1 second.
+    # get response from server
+    modifiedMessage, addr = clientSocket.recvfrom(2048)
+    RTT = int(round(time.time() * 1000)) - RTT    # end time - start time (i.e. RTT in ms)
+    print(modifiedMessage.decode(), addr, "\nRTT: " + str(RTT) + "ms");
+    clientSocket.close()
+except timeout:
+    print("Error: Response timeout")
+
+
+#Allow the client to give up if no response has been received within 1 second.
--- UDP/UDPServer.py
+++ UDP/UDPServer.py
@@ -3,21 +3,27 @@
 #UDP (SOCK_DGRAM) is a datagram-based protocol. You send one 
 #datagram and get one reply and then the connection terminates.
 from socket import socket, SOCK_DGRAM, AF_INET
+import random
 
 #Create a UDP socket 
 #Notice the use of SOCK_DGRAM for UDP packets
 #AF_INET is the Internet address family for IPv4
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+
 # Assign IP address and port number to socket
 serverSocket.bind(('', 12000))
-print "Waiting for connections"
+print("Waiting for connections")
+
 while True:
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(2048)
-    # Capitalize the message from the client
-    print message, address
-    message = message.upper()
-    serverSocket.sendto(message, address)
+    if random.random() < 0.5:
+        # Capitalize the message from the client and send
+        serverSocket.sendto(message.upper(), address)
+        print(message.decode(), address)  # decoding bytes message to string
+    else:
+        print("packet lost")    # "lose" 50% of received packets
+
 serverSocket.close()
 
 
